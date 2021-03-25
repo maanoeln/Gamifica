0c13e23ca767dd0d15390cd7b3c2e44fc6e61892
Manoel Neto <mneto_004@hotmail.com>
2021-03-25 19:10:07 -0300

    feat: create global styled for app

---
+++ src/global/globalStyled.js
@@ -0,0 +1,25 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -o-box-sizing: border-box;
+
+    ::-webkit-inner-spin-button, 
+    ::-webkit-outer-spin-button { 
+      -webkit-appearance: none; 
+      margin: 0; 
+    }
+  }
+`;
+
+const AppContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export { GlobalStyle, AppContainer };
