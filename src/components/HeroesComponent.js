1d2511955d17f8f18deb0fab17d1cf48c8482a7c
Manoel Neto <mneto_004@hotmail.com>
2021-03-25 20:06:32 -0300

    feat: add new props to container style and some cards

--- src/components/HeroesComponent.js
+++ src/components/HeroesComponent.js
@@ -3,16 +3,35 @@ import styled from 'styled-components';
 import HeroesCardComponent from './HeroesCardComponent';
 
 const Container = styled.div`
-  width: 50%;
+  margin-top: 16px;
+  width: 80%;
+  min-height: 100%;
   display: grid;
-  gap: 16px;
-  grid-template-columns: repeat(3, 1fr);
-  margin: auto;
+  gap: 24px;
+  grid-template-columns: repeat(5, 1fr);
+  margin: 16px auto 16px;
 `;
 
 const HeroesComponent = () => {
   return (
     <Container>
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
+      <HeroesCardComponent />
       <HeroesCardComponent />
     </Container>
   );
