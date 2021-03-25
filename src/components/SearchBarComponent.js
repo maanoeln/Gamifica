dbf89c6f7df34571ef92dd2dfed51f0437b6927a
Manoel Neto <mneto_004@hotmail.com>
2021-03-25 20:07:10 -0300

    feat: change the search container to be a sticky to the top

--- src/components/SearchBarComponent.js
+++ src/components/SearchBarComponent.js
@@ -3,17 +3,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SearchContainer = styled.div`
-  position: relative;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
   width: 100%;
-  top: 100px;
+  top: 0;
   height: 100px;
   display: flex;
   align-items: center;
   justify-content: center;
+  z-index: 99;
+  background-color: #292929;
+  box-shadow: 0 3px 20px rgba(0, 0, 0, 0.26);
+  margin-bottom: 16px;
+  color: #fffffc;
 `;
 
 const SearchInputContainer = styled.div`
-  border-bottom: 1px solid #292929;
+  border-bottom: 1px solid #fffffc;
   outline: none;
   width: 30%;
   display: flex;
@@ -25,6 +31,8 @@ const SearchInputContainer = styled.div`
     border: none;
     font-size: 24px;
     margin-left: 8px;
+    color: #fffffc;
+    background-color: transparent;
   }
 `;
 
