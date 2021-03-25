60bacc3a5f5fbb552684386fefe35695f3bc506d
Manoel Neto <mneto_004@hotmail.com>
2021-03-25 19:27:43 -0300

    feat: add serachbar to component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchBarComponent from './components/SearchBarComponent';
 import TopHeaderComponent from './components/TopHeaderComponent';
 import { AppContainer, GlobalStyle } from './global/globalStyled';
 
@@ -7,6 +8,7 @@ const App = () => {
     <AppContainer>
       <GlobalStyle />
       <TopHeaderComponent />
+      <SearchBarComponent />
     </AppContainer>
   );
 };
