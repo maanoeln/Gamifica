2ab833cc0f3077422b41af2fbbfc5e4e563d948c
Manoel Neto <mneto_004@hotmail.com>
2021-03-25 22:36:26 -0300

    feat: add new props and prop types to heroes component

--- src/components/HeroesComponent.js
+++ src/components/HeroesComponent.js
@@ -1,9 +1,15 @@
-import PropTypes from 'prop-types';
+import { PropTypes } from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 import HeroesCardComponent from './HeroesCardComponent';
+import SearchBarComponent from './SearchBarComponent';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Grid = styled.div`
   margin-top: 16px;
   width: 80%;
   min-height: 100%;
@@ -13,18 +19,23 @@ const Container = styled.div`
   margin: 16px auto 16px;
 `;
 
-const HeroesComponent = ({ data }) => {
+const HeroesComponent = ({ data, handleChange, value }) => {
   return (
     <Container>
-      {data.map(({ id, ...d }) => (
-        <HeroesCardComponent key={id} data={d} />
-      ))}
+      <SearchBarComponent handleChange={handleChange} value={value} />
+      <Grid>
+        {data.map(({ id, ...d }) => (
+          <HeroesCardComponent key={id} data={d} />
+        ))}
+      </Grid>
     </Container>
   );
 };
 
 HeroesComponent.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
 };
 
 export default HeroesComponent;
