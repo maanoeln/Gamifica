e4e9215356fa2fef2ef789c03cdd7ac0f3f4960d
Manoel Neto <mneto_004@hotmail.com>
2021-03-25 23:26:41 -0300

    feat: create functions to filter and sort characters by name

--- src/containers/HeroesContainer.js
+++ src/containers/HeroesContainer.js
@@ -2,9 +2,24 @@ import React, { useEffect, useState } from 'react';
 import HeroesComponent from '../components/HeroesComponent';
 import api from '../services/api';
 
+const sortData = (sort) => (characterOne, characterTwo) => {
+  const name1 = characterOne.name;
+  const name2 = characterTwo.name;
+  return sort && name1.localeCompare(name2);
+};
+
+const filterAndSortProducts = ({ data, search, sort }) =>
+  data
+    .filter((d) => {
+      const productName = d.name.toLowerCase();
+      return productName.indexOf(search.toLowerCase()) > -1;
+    })
+    .sort(sortData(sort));
+
 const HeroesContainer = () => {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState('');
+  const [sort, setSort] = useState(false);
 
   useEffect(() => {
     api
@@ -13,20 +28,17 @@ const HeroesContainer = () => {
       .catch((error) => console.log(error));
   }, []);
 
-  const filteredProducts = data.filter((d) => {
-    const productName = d.name.toLowerCase();
-    return productName.indexOf(search.toLowerCase()) > -1;
-  });
-
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
 
   return (
     <HeroesComponent
-      data={filteredProducts}
+      data={filterAndSortProducts({ data, search, sort })}
       handleChange={(e) => handleChange(e)}
       value={search}
+      sortData={() => setSort((state) => !state)}
+      sort={sort}
     />
   );
 };
