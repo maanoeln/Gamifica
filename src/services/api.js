3878859ad64b8a33e8b1d06fcb61c90376be685a
Manoel Neto <mneto_004@hotmail.com>
2021-03-25 22:19:40 -0300

    feat: create api service

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://gateway.marvel.com/v1/public',
+  params: {
+    apikey: '90dd7684a5f699c58a5b4b6432ed3086',
+  },
+});
+
+export default api;
