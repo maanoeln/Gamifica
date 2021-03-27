323ebd843253bc4ea35968b045d9f41a855e3737
Manoel Neto <mneto_004@hotmail.com>
2021-03-27 14:35:10 -0300

    feat: create routes component

---
+++ src/routes/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HeroesContainer from '../containers/HeroesContainer';
+import HeroesInfoContainer from '../containers/HeroesInfoContainer';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path={'/'} component={HeroesContainer} />
+      <Route exact path={'/character/:id'} component={HeroesInfoContainer} />
+    </Switch>
+  );
+};
+
+export default Routes;
