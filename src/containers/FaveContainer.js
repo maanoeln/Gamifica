de0639306e097d19d159d8f3e76b4b43d4ad2b8d
Manoel Neto <mneto_004@hotmail.com>
2021-03-27 14:36:11 -0300

    feat: create fave container with functions

---
+++ src/containers/FaveContainer.js
@@ -0,0 +1,16 @@
+import { PropTypes } from 'prop-types';
+import React from 'react';
+import FaveComponent from '../components/FaveComponent';
+import { useFave } from '../context/FavesProvider';
+
+const FaveContainer = ({ id }) => {
+  const { handleFaves, isFave } = useFave();
+
+  return <FaveComponent isFave={isFave(id)} handleFaves={handleFaves(id)} />;
+};
+
+FaveContainer.propTypes = {
+  id: PropTypes.number.isRequired,
+};
+
+export default FaveContainer;
