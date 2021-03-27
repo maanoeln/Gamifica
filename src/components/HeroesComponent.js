b25e9e0be3a197c53976b027352915888cd695f2
Manoel Neto <mneto_004@hotmail.com>
2021-03-27 16:31:53 -0300

    feat: add responsiveness to heroes component

--- src/components/HeroesComponent.js
+++ src/components/HeroesComponent.js
@@ -17,6 +17,22 @@ const Grid = styled.div`
   gap: 24px;
   grid-template-columns: repeat(5, 1fr);
   margin: 16px auto;
+
+  @media (min-width: 320px) and (max-width: 549px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  @media (min-width: 550px) and (max-width: 809px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 810px) and (max-width: 1099px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1100px) and (max-width: 1300px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
 
 const HeroesComponent = ({
