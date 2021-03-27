0e3afb6c37e62305824dd59396cb288b017a9c53
Manoel Neto <mneto_004@hotmail.com>
2021-03-27 14:35:33 -0300

    feat: add provider to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import TopHeaderComponent from './components/TopHeaderComponent';
-import HeroesContainer from './containers/HeroesContainer';
-import { AppContainer, GlobalStyle } from './global/globalStyled';
+import { FaveProvider } from './context/FavesProvider';
+import { GlobalStyle } from './global/globalStyled';
+import history from './routes/history';
+import Routes from './routes/routes';
 
 const App = () => {
   return (
-    <AppContainer>
-      <GlobalStyle />
-      <TopHeaderComponent />
-
-      <HeroesContainer />
-    </AppContainer>
+    <FaveProvider>
+      <Router history={history}>
+        <GlobalStyle />
+        <TopHeaderComponent />
+        <Routes />
+      </Router>
+    </FaveProvider>
   );
 };
 
