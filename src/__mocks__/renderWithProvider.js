528464c5cad3cd4fd7a5ecc8a777f4cebba962ad
Manoel Neto <mneto_004@hotmail.com>
2021-03-28 18:34:46 -0300

    feat: create render with provider component for test

---
+++ src/__mocks__/renderWithProvider.js
@@ -0,0 +1,50 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import TopHeaderContainer from '../containers/TopHeaderContainer';
+import { FaveProvider } from '../context/FavesProvider';
+
+const renderComponent = (
+  WrappedComponent,
+  {
+    match = { params: {} },
+    history = { goBack: jest.fn(), push: jest.fn() },
+    ...props
+  },
+  fn = render
+) => {
+  return fn(
+    <FaveProvider>
+      <BrowserRouter>
+        <MemoryRouter>
+          <TopHeaderContainer />
+          <WrappedComponent history={history} match={match} {...props} />
+          <ToastContainer style={{ zIndex: 999, opacity: 1 }} />
+        </MemoryRouter>
+      </BrowserRouter>
+    </FaveProvider>
+  );
+};
+
+export const renderAppComponent = (
+  WrappedComponent,
+  {
+    match = { params: {} },
+    history = { goBack: jest.fn(), push: jest.fn() },
+    ...props
+  },
+  fn = render
+) => {
+  return fn(
+    <FaveProvider>
+      <BrowserRouter>
+        <MemoryRouter>
+          <WrappedComponent history={history} match={match} {...props} />
+          <ToastContainer style={{ zIndex: 999, opacity: 1 }} />
+        </MemoryRouter>
+      </BrowserRouter>
+    </FaveProvider>
+  );
+};
+
+export default renderComponent;
