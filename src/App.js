35f0b6a89de68aad0d1358c64f6f94ba25b19a1a
Manoel Neto <mneto_004@hotmail.com>
2021-03-28 18:40:21 -0300

    feat: add toast to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import TopHeaderContainer from './containers/TopHeaderContainer';
 import { FaveProvider } from './context/FavesProvider';
 import { GlobalStyle } from './global/globalStyled';
@@ -13,6 +15,7 @@ const App = () => {
         <GlobalStyle />
         <TopHeaderContainer />
         <Routes />
+        <ToastContainer style={{ zIndex: 999, opacity: 1 }} />
       </Router>
     </FaveProvider>
   );
