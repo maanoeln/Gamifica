9fdcf389e3a5942d9c18206c03f216e15afb23d7
Manoel Neto <mneto_004@hotmail.com>
2021-03-28 18:35:14 -0300

    feat: create mocks in setup test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+export const mockHistory = {
+  replace: jest.fn(),
+  push: jest.fn(),
+  goBack: jest.fn(),
+};
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useHistory: () => mockHistory,
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => mockHistory,
+}));
