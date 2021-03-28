be136f9ad2edb323b769963c5a838fce52aecd7a
Manoel Neto <mneto_004@hotmail.com>
2021-03-28 18:35:43 -0300

    feat: create data mock

---
+++ src/__mocks__/data.js
@@ -0,0 +1,138 @@
+export const data = {
+  data: {
+    total: 1500,
+    results: [
+      {
+        id: 1,
+        name: '3-D Man',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 2,
+        name: 'A-Bomb',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+
+      {
+        id: 3,
+        name: 'A.I.M',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 4,
+        name: 'Aaron Stack',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 5,
+        name: 'Abomination',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+
+      {
+        id: 6,
+        name: 'Abomination Ultimate',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 7,
+        name: 'Absorbing man',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 8,
+        name: 'Abyss',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+
+      {
+        id: 9,
+        name: 'Abyss Apocalipse',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 12,
+        name: 'Aegis',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 13,
+        name: 'Agent Brand',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 14,
+        name: 'Agent X',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+
+      {
+        id: 15,
+        name: 'Agent Zero',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 16,
+        name: 'Agents of Atlas',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 17,
+        name: 'Aginar',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+
+      {
+        id: 18,
+        name: 'Air walker',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 19,
+        name: 'Ajak',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 20,
+        name: 'Ajaxis',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+
+      {
+        id: 21,
+        name: 'Amun',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 22,
+        name: 'Ancient One',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 23,
+        name: 'Ancient One Ultimate',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 24,
+        name: 'Angel',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 25,
+        name: 'Angel Ultimate',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 10,
+        name: 'Adam Destine',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+      {
+        id: 11,
+        name: 'Adam Warlock',
+        thumbnail: { extension: 'jpeg', path: 'www.path.com' },
+      },
+    ],
+  },
+};
